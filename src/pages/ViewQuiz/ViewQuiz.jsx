34e945b326e9dc4f9ca0519fcdd13352744545da
jayani wickramathilaka <Jayani Saumya>
2022-05-05 06:00:20 +0530

    quiz static display

--- src/pages/ViewQuiz/ViewQuiz.jsx
+++ src/pages/ViewQuiz/ViewQuiz.jsx
@@ -1,13 +1,9 @@
+
 import React from "react";
 import { Grid } from "@mui/material";
-import "./ViewCourse.css";
+import "./ViewQuiz.css";
 import CustomAccordion from "../../components/Accordion/Accordion";
 import CustomTab from "../../components/Tab/CustomTab";
-import { RadioGroup } from '@mui/material';
-import { FormControl } from '@mui/material';
-import { FormLabel } from '@mui/material';
-import { FormControlLabel } from '@mui/material';
-import { Radio } from '@mui/material';
 
 const tabs = [
   {
@@ -27,29 +23,28 @@ const tabs = [
     body: <div>Review</div>,
   },
 ];
-const ViewCourse = () => {
+const ViewQuiz = () => {
   return (
     <div>
       <Grid container>
         <Grid item xs={9}>
-          {/* <video className="video_preview" controls src="" /> */}
-          <div className="quiz_container">
+         <div className="quiz_container">
          <FormControl>
                 <FormLabel id="demo-radio-buttons-group-label">How familiar are you with Force?</FormLabel>
-                <br></br>
                 <RadioGroup
                     aria-labelledby="demo-radio-buttons-group-label"
                     // defaultValue="female"
                     name="radio-buttons-group"
                 >
                     <FormControlLabel value="neverHeard" control={<Radio />} label="I have never heard of it" />
-                    <FormControlLabel value="heard" control={<Radio />} label="I have heard of it, but don't know what is it" />
+                    <FormControlLabel value="" control={<Radio />} label="I have heard of it, but don't know what is it" />
                     <FormControlLabel value="" control={<Radio />} label="I have some idea of it, but it's not very clear" />
                     <FormControlLabel value="" control={<Radio />} label="I know what is it and could explain what it's used for" />
                     <FormControlLabel value="" control={<Radio />} label="I know what it is and when to use it, and I could use it to analyze data" />
                 </RadioGroup>
                 </FormControl>
          </div>
+
           <CustomTab tabs={tabs} />
         </Grid>
         <Grid item xs={3} className="course-content">
@@ -60,4 +55,4 @@ const ViewCourse = () => {
     </div>
   );
 };
-export default ViewCourse;
+export default ViewQuiz;
