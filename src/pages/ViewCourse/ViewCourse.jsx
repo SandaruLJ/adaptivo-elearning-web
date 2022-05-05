d29b347964584c77c15e460ff86bbd44b44e92ef
Rishy20 <58569021+Rishy20@users.noreply.github.com>
2022-05-05 16:03:11 +0530

    Implement View Course Interface

--- src/pages/ViewCourse/ViewCourse.jsx
+++ src/pages/ViewCourse/ViewCourse.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Grid } from "@mui/material";
 import "./ViewCourse.css";
 import CustomAccordion from "../../components/Accordion/Accordion";
 import CustomTab from "../../components/Tab/CustomTab";
+import { useFetch } from "../../components/useFetch";
+import { getCourseById } from "../../service/course.service";
 
 const tabs = [
   {
@@ -22,17 +24,33 @@ const tabs = [
     body: <div>Review</div>,
   },
 ];
+
 const ViewCourse = () => {
+  const [data, setData] = useState();
+  const [main, setMain] = useState({
+    type: "",
+    body: "",
+  });
+  const getData = async () => {
+    //Fetches the data from the db
+    const response = await getCourseById("6269d0c1fac8add4e331dbb7");
+    console.log(response);
+    setData(response);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div>
       <Grid container>
         <Grid item xs={9}>
-          <video className="video_preview" controls src="" />
-          <CustomTab tabs={tabs} />
+          <div className="view-course-main-container">{main.type == "video" && <video className="video_preview" controls src={main.body} />}</div>
+          {data && <CustomTab tabs={tabs} />}
         </Grid>
         <Grid item xs={3} className="course-content">
           <div className="course-content-title">Course Content</div>
-          <CustomAccordion />
+          {data && <CustomAccordion curriculum={data.curriculum} setMain={setMain} />}
         </Grid>
       </Grid>
     </div>
